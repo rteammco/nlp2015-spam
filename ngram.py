eead5a91eae0f96dfaed5cf3a56a2f86924e10f5
Richard Teammco <teammco@cs.utexas.edu>
2015-04-24 15:48:43 -0500

    Preprocessor now correctly removes spaces from characters.

--- ngram.py
+++ ngram.py
@@ -1,31 +1,38 @@
-from nltk.probability import LidstoneProbDist
-from nltk.probability import SimpleGoodTuringProbDist
-from nltk.model import NgramModel
+from collections import defaultdict
+import nltk
+from nltk.util import ngrams
 
 
-def train_model(data, N):
+class NGramModel():
+    def __init__(self):
+        pass
+
+    def add_sentence(self, sentence):
+        pass
+    
+    def compute_probabilities(self):
+        pass
+
+def train_model(data, N, frequencies):
     """
     Trains an N-gram model (N is a parameter) on the given data (list of raw
     email message strings). The model is then returned.
     The provided training data should consist of a type of message (i.e. only
     ham messages or only spam messages). It should be a list of full messages.
     """
-    # convert data into a single list of words
-    data = map(str.split, data)
-    data = [item for sublist in data for item in sublist]
-    estimator = lambda fdist, bins: LidstoneProbDist(fdist, 1)
-    model = NgramModel(N, data, estimator=estimator)
-    return model
+    for sentence in data:
+        words = ['<S>'] + nltk.word_tokenize(sentence) + ['</S>']
+        ngs = ngrams(words, N)
+        for ng in ngs:
+            frequencies[ng] += 1
 
 
 s1 = 'this is a test sentence and it is very good without a double the test sentence is good'
 s2 = 'this is another sentence and I would like to also test it please'
-
-data = [s1, s2]
-model = train_model(data, 3)
-print model.prob('test', ['also'])
-print model.prob('test', ['a'])
-print model.prob('thinkwould', ['would'])
+freq = defaultdict(int)
+train_model([s1], 2, freq)
+train_model([s2], 2, freq)
+print freq
 exit(0)
 
 
