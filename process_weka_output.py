44a249af9f8edcb9da5d48c687ca9bf3e7297263
Richard Teammco <teammco@cs.utexas.edu>
2015-05-13 16:58:37 -0500

    Added script to give precision/recall from Weka output.

---
+++ process_weka_output.py
@@ -0,0 +1,47 @@
+import sys
+
+
+def process_input(in_lines, predicted_class):
+    """
+    Extract the accuracy/precision/recall from the Weka output.
+    Prints the original lines of the output, but also returns the
+    a/p/r values.
+    """
+    num_correctly_predicted = 0
+    num_predicted = 0
+    num_actual = 0
+    num_total = 0
+    num_correct = 0
+    for line in in_lines:
+        if len(line) == 0 or line.startswith('='):
+            continue
+        parts = line.split()
+        if not parts[0].isdigit():
+            continue
+        actual = parts[1].split(':')[1]
+        predicted = parts[2].split(':')[1]
+        if actual == predicted:
+            num_correct += 1
+        if actual == predicted and predicted == predicted_class:
+            num_correctly_predicted += 1
+        if predicted == predicted_class:
+            num_predicted += 1
+        if actual == predicted_class:
+            num_actual += 1
+        num_total += 1
+    accuracy = float(num_correct) / float(num_total)
+    precision = float(num_correctly_predicted) / float(num_predicted)
+    recall = float(num_correctly_predicted) / float(num_actual)
+    return accuracy, precision, recall
+
+
+if __name__ == '__main__':
+    in_lines = []
+    for line in sys.stdin:
+        line = line.strip()
+        print line
+        in_lines.append(line)
+    accuracy, precision, recall = process_input(in_lines, 'spam')
+    print "\n\n"
+    print "Accuracy / Precision / Recall:"
+    print str(accuracy) + " / " + str(precision) + " / " + str(recall)
